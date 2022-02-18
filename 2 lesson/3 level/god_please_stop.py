4f1846b21c1c567b28e718b431419563acf899b5
Aleksander Tsurkan <a.tsurkan@systtech.ru>
2022-02-18 09:39:19 +0200

    2 lesson - hard level

--- 2 lesson/3 level/god_please_stop.py
+++ 2 lesson/3 level/god_please_stop.py
@@ -6,25 +6,24 @@ class Game:
         self.number = int(random.randint(1, 10))
 
     def validate(self, guess: int):
-          if self.guess < self.number:
-              val = '\nТвое число меньше загаданного.'
-              print(val)
-              return False
-          elif self.guess > self.number:
-              val = '\nТвое число больше загаданного.'
-              print(val)
-              return False
-          else:
-              val = '\nУгадал!'
-              print(val)
-              return True
+        if self.guess < self.number:
+            val = '\nТвое число меньше загаданного.'
+            print(val)
+            return False
+        elif self.guess > self.number:
+            val = '\nТвое число больше загаданного.'
+            print(val)
+            return False
+        else:
+            val = '\nУгадал!'
+            print(val)
+            return exit(0)
 
     def mainGame(self):
         print('Угадай число между 1 и 10\n')
         while True:
-          self.guess = int(input('\nВведи число: \n'))
-          if self.validate(self.guess):
-            break
+            self.guess = int(input('\nВведи число: \n'))
+            self.validate(self.guess)
 
 
 c = Game()
