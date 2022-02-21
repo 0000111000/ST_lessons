a13b6abda584b4bd086fcfd8cdb88107d5711dad
Aleksander Tsurkan <a.tsurkan@systtech.ru>
2022-02-21 03:14:55 +0200

    3 lesson - merge games

--- 3 lesson/main.py
+++ 3 lesson/main.py
@@ -3,7 +3,13 @@ from random import randint
 
 class GAME1:
     def __init__(self):
-        self.number = int(randint(1, 10))
+        self.num1 = int(1)
+        self.num2 = int(10)
+        if game_mode == 1:
+            self.number = int(randint(self.num1, self.num2))
+        else:
+            num = int()
+
 
     def validate(self, guess: int):
         if self.guess < self.number:
@@ -19,19 +25,29 @@ class GAME1:
             print(val)
             return exit(0)
 
-    def maingame(self):
-        print('Угадай число между 1 и 10!')
-        while True:
-            self.guess = int(input('Введи число:'))
-            self.validate(self.guess)
+    def engine(self):
+        if game_mode == 1:
+            print('\nУгадай число между 1 и 10!')
+            while True:
+                self.guess = int(input('Введи число:'))
+                self.validate(self.guess)
+        else:
+            while True:
+                current_num = randint(self.num1, self.num2)
+                print("Пытаюсь отгадать (от 1 до 10), твое число :", current_num, "?")
+                enter = input("\n Угадал '=', больше '>' ,меньше '<':")
+                if enter == '>':
+                    self.num1 = current_num + 1
+                    GAME2.validate(self.num1)
+                elif enter == '<':
+                    self.num2 = current_num - 1
+                    GAME2.validate(self.num2)
+                elif enter == '=':
+                    print('Я угадал, твое число: ' + str(current_num))
+                    return True
 
 
-class GAME2:
-    def __init__(self):
-        self.num1 = int(1)
-        self.num2 = int(10)
-
-
+class GAME2(GAME1):
     @staticmethod
     def validate(num):
         if not 0 < num < 11:
@@ -41,23 +57,14 @@ class GAME2:
             return False
 
 
-    def maingame(self):
-        while True:
-            current_num = randint(self.num1, self.num2)
-            print(current_num)
-            enter = input("Загадайте число от 1 до 10\n Угадал '=', больше '>' ,меньше '<':")
-            if enter == '>':
-                self.num1 = current_num + 1
-                self.validate(self.num1)
-            elif enter == '<':
-                self.num2 = current_num - 1
-                self.validate(self.num2)
-            elif enter == '=':
-                print('Я угадал, твое число: ' + str(current_num))
-                return True
-
-
-run = GAME1()
-run.maingame()
-
+game_mode = int(input('Выбери режим игры: \n 1. Я загадываю число \n 2. Ты загадываешь число\n\n '))
+if game_mode == 1:
+    run = GAME1()
+    run.engine()
+elif game_mode == 2:
+    run = GAME2()
+    run.engine()
+else:
+    print('Некорректный ввод! Аварийное завершение')
+    exit(0)
 
