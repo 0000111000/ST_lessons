64083f20a53741eaf493c8c6de57e49076d41c5d
Aleksander Tsurkan <a.tsurkan@systtech.ru>
2022-02-21 13:18:05 +0200

    3 lesson - merge games

--- 3 lesson/main.py
+++ 3 lesson/main.py
@@ -3,24 +3,21 @@ from random import randint
 
 class GAME1:
     def __init__(self):
-        self.num1 = int(1)
-        self.num2 = int(10)
-        if game_mode == 1:
-            self.number = int(randint(self.num1, self.num2))
-
-
+        self.num1: int = int(1)
+        self.num2: int = int(10)
+        self.number: int = int(randint(self.num1, self.num2))
 
     def validate(self, guess: int):
         if self.guess < self.number:
-            val = '\nТвое число меньше загаданного.'
+            val: str = '\nТвое число меньше загаданного.'
             print(val)
             return False
         elif self.guess > self.number:
-            val = '\nТвое число больше загаданного.'
+            val: str = '\nТвое число больше загаданного.'
             print(val)
             return False
         else:
-            val = '\nУгадал!'
+            val: str = '\nУгадал!'
             print(val)
             return exit(0)
 
@@ -28,13 +25,13 @@ class GAME1:
         if game_mode == 1:
             print('\nУгадай число между 1 и 10!')
             while True:
-                self.guess = int(input('Введи число:'))
+                self.guess: int = int(input('Введи число:'))
                 self.validate(self.guess)
         else:
             while True:
-                current_num = randint(self.num1, self.num2)
+                current_num: int = randint(self.num1, self.num2)
                 print("Пытаюсь отгадать (от 1 до 10), твое число :", current_num, "?")
-                enter = input("\n Угадал '=', больше '>' ,меньше '<':")
+                enter: str = input("\n Угадал '=', больше '>' ,меньше '<':")
                 if enter == '>':
                     self.num1 = current_num + 1
                     GAME2.checking(self.num1)
@@ -48,7 +45,7 @@ class GAME1:
 
 class GAME2(GAME1):
     @staticmethod
-    def checking(num):
+    def checking(num: int):
         if not 0 < num < 11:
             print('Обманываешь!')
             exit(0)
@@ -56,7 +53,7 @@ class GAME2(GAME1):
             return False
 
 
-game_mode = int(input('Доступные игры: \n 1. Я загадываю число \n 2. Ты загадываешь число\n\n Ввод режима: '))
+game_mode: int = int(input('Доступные игры: \n 1. Я загадываю число \n 2. Ты загадываешь число\n\n Ввод режима: '))
 if game_mode == 1:
     run = GAME1()
     run.engine()
